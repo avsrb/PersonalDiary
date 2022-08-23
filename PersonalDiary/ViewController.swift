c03d4e80c6d430452a3a07b1f85ddd873196204e
Artem <Serebryakov>
2022-08-23 13:50:03 +0300

    Initial Commit

---
+++ PersonalDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalDiary
+//
+//  Created by Artem Serebriakov on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
